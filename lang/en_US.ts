94a654e868e42f13aff36df4e42c58ae5abcfcfc
liszd <lidzd1992@gail.com>
2013-10-12 17:12:24 +0800

    commit

---
+++ lang/en_US.ts
@@ -0,0 +1,352 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US" sourcelanguage="en_US">
+<context>
+    <name>HostsUtlMain</name>
+    <message>
+        <location filename="qthostsui.py" line="268"/>
+        <source>Backup hosts</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="267"/>
+        <source>Functions</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="258"/>
+        <source>Hosts Setup Utility</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="259"/>
+        <source>Config</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="260"/>
+        <source>Server</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="261"/>
+        <source>IP Version</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="262"/>
+        <source>Status</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="263"/>
+        <source>Connection</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="290"/>
+        <source>N/A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="265"/>
+        <source>OS</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="269"/>
+        <source>Backup the hosts file of current system.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="270"/>
+        <source>Download data file</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="271"/>
+        <source>Download the latest data file.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="272"/>
+        <source>Restore backup</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="273"/>
+        <source>Restore a previous backup of hosts file.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="274"/>
+        <source>Apply hosts</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="275"/>
+        <source>Apply changes to the hosts file.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="276"/>
+        <source>Exit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="277"/>
+        <source>Close this tool.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="278"/>
+        <source>Check update / Refresh</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="279"/>
+        <source>Check the latest version of hosts data file.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="284"/>
+        <source>Hosts Info</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="285"/>
+        <source>Version</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="287"/>
+        <source>Release</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="289"/>
+        <source>Latest</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="142"/>
+        <source>google(cn)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="143"/>
+        <source>google(us)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="144"/>
+        <source>activation-helper</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="145"/>
+        <source>others</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="146"/>
+        <source>adblock-hostsx</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="147"/>
+        <source>adblock-mvps</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="148"/>
+        <source>adblock-mwsl</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="149"/>
+        <source>adblock-yoyo</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="519"/>
+        <source>Building hosts file...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="321"/>
+        <source>Backup File(*.bak)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="321"/>
+        <source>Restore hosts</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="1482"/>
+        <source>[Error]</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="674"/>
+        <source>Checking...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="538"/>
+        <source>Copying new hosts file to
+  %s</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="543"/>
+        <source>Remove temporary file</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="1107"/>
+        <source>Operation completed</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="678"/>
+        <source>[OK]</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="681"/>
+        <source>[Failed]</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="862"/>
+        <source>Applying module: %s(%s/%s)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="882"/>
+        <source>Progress</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="912"/>
+        <source>Notice: %i hosts entries has 
+  been applied in %ssecs.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="916"/>
+        <source>Operation Completed Successfully!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="961"/>
+        <source>Error</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="1045"/>
+        <source>Incorrect Data file!
+Please use the &quot;Download&quot; key to 
+fetch a new data file.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="981"/>
+        <source>Download Complete</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="1033"/>
+        <source>Warning</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="1019"/>
+        <source>You do not have permissions to change the 
+hosts file.
+Please run this program as Administrator/root
+so it can modify your hosts file.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="1033"/>
+        <source>Error retrieving data from the server.
+Please try another server.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="1060"/>
+        <source>Data file not found!
+Please use the &quot;Download&quot; key to 
+fetch a new data file.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="1098"/>
+        <source>Notice</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="1098"/>
+        <source>Data file is up-to-date.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="1107"/>
+        <source>Complete</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="1202"/>
+        <source>Connecting...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="1242"/>
+        <source>Downloading: %s / %s</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="1079"/>
+        <source>Are you sure you want to apply changes 
+to the hosts file on your system?
+
+This operation could not be reverted if 
+you have not made a backup of your 
+current hosts file.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="280"/>
+        <source>Save with ANSI</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="281"/>
+        <source>Export to hosts file encoding by ANSI.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="282"/>
+        <source>Save with UTF-8</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="qthostsui.py" line="283"/>
+        <source>Export to hosts file encoding by UTF-8.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="496"/>
+        <source>Export hosts</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="hostsutl.py" line="496"/>
+        <source>hosts File</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
